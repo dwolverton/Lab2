edb05c82d63580a88ea752384e6381271f408e67
David Wolverton <dwolvert@gmail.com>
2020-10-13 10:35:34 -0400

    more exclamation

--- src/lab2/RoomCalculatorApp.java
+++ src/lab2/RoomCalculatorApp.java
@@ -3,6 +3,6 @@ package lab2;
 public class RoomCalculatorApp {
 
 	public static void main(String[] args) {
-		System.out.println("Finished lab 2.");
+		System.out.println("Finished lab 2!");
 	}
 }
